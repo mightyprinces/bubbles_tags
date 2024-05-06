d47d2835429f38c5e497b5842f106054b016ba5d
mightyprinces <happyrabbit62@gmail.com>
2024-05-07 00:59:27 +0200

    fix: redundant initial tags, blank tags, click on empty space put focus into input, add max width for tag

--- bubbles.js
+++ bubbles.js
@@ -1,12 +1,9 @@
 class Bubbles {
   constructor(wrapperEl, tagsChangedHandler) { // wrapperEl в данном случае это bubblesFirstWrapperEl из index.html
-
     this.tags = []; // массив текущих тегов
     this.tagsChangedHandler = tagsChangedHandler;
-
-    this.addTag('test1');
     this.createLayout(wrapperEl); // код запускающий createLayout. wrapperEl в данном случае это bubblesFirstWrapperEl из index.html
-    this.addTag('test');
+    // this.addTag('test');
   }
 
   /**
@@ -30,8 +27,14 @@ class Bubbles {
         tagInputEl.value = '';
       }
     })
-    // tagInputEl.focus();
+    
     this.tagsAndInputWrapperEl.appendChild(tagInputEl);
+
+    this.tagsAndInputWrapperEl.addEventListener('click', (ev) => {
+      console.log(ev);
+      if (ev.target === this.tagsAndInputWrapperEl) tagInputEl.focus();
+    })
+
     wrapperEl.appendChild(this.tagsAndInputWrapperEl);
   }
 
@@ -52,17 +55,19 @@ class Bubbles {
       !this.tagsWrapperEl  // проверка что tagsWrapperEl существует
       || tagValue === ''   // и что мы не создаем пустой тэг, так как value инпута не пустой
       || this.tags.includes(tagValue)
+      || tagValue.trim() === ''
     ) return;
     
     const value = tagValue.trim();
     const tagEl = document.createElement('div');
     tagEl.classList.add('tag-bubble');
-    const tagContent = document.createTextNode(value);
-    tagEl.appendChild(tagContent);
+    const tagTextEl = document.createElement('div');
+    tagTextEl.classList.add('tag-text');
+    tagTextEl.textContent = value;
+    tagEl.appendChild(tagTextEl);
 
     const removeEl = document.createElement('span');
     removeEl.classList.add('remove-tag');
-
     removeEl.addEventListener('click', () => {
       this.removeTag(tagEl);
     })
