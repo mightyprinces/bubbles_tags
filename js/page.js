f27f51f26da543c1ebfff74d0a8dfe1eb5f9604d
mightyprinces <happyrabbit62@gmail.com>
2024-05-14 03:33:23 +0300

    add use of local storage for saving tags

---
+++ js/page.js
@@ -0,0 +1,19 @@
+import Bubbles from '/js/lib/bubbles.js';
+import Storage from '/js/lib/storage.js';
+
+class Page {
+  static init() {
+    const bubblesFirstWrapperEl = document.querySelector('.bubbles_wrapper--first');
+    const initTagsArr = Storage.loadTags();
+
+    // хендлер должен запускаться когда мы добавляем или удаляем тег
+    function tagsChangedHandler(tags) {
+      console.log('Something changed:', tags);
+      Storage.saveTags(tags);
+    }
+
+    new Bubbles(bubblesFirstWrapperEl, tagsChangedHandler, initTagsArr);
+  }
+}
+
+Page.init();
